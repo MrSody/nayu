ff6adb917f3cd1bb0a2065dbcae7352364649e89
Cristian Cuartas <kristian_9411@hotmail.com>
2023-09-18 18:02:45 -0500

    add

--- code.js
+++ code.js
@@ -1,13 +1,23 @@
 window.addEventListener("load", function (event) {
-    if (validateStart()) {
-        main();
+    let cookie = obtenerCookie();
+
+    if (!validateStart()) {
+        this.document.getElementById("body").classList.add("fondoError");
+        this.document.getElementById("main").classList.add("hidden");
+    }
+    
+    if (cookie != null){
+        NUMCUESTIONNOW = cookie;
+        loadCuestion(NUMCUESTIONNOW);
     }
 });
 
-let NUMCUESTIONNOW = 0;
+let NUMCUESTIONNOW = 1;
+let amorWidth = 70;
+let amorHeight = 50;
+let tristezaWidth = 90;
+let tristezaHeight = 50;
 
-// TEXTO original = 2034na7423yu4298
-// TEXTO Cifrado = 2034qd7423bx4298
 function validateStart () {
     const urlParams = new URLSearchParams(window.location.search);
 
@@ -44,13 +54,16 @@ function cifrar(mensaje, desplazamiento) {
     return mensajeCifrado;
 }
 
-function main() {
+function startCuestions(){
+    document.getElementById("start").classList.add("hidden");
+    document.getElementById("preguntas").classList.remove("hidden");
     loadCuestion(NUMCUESTIONNOW);
 }
 
 function loadSection (dataSection) {
-    document.getElementById("pregunta").innerHTML = `<p>${dataSection.pregunta}</p>`;
-    var btnValidate = document.getElementById("btnValidate");
+    document.getElementById("Pregunta").innerHTML = `<h1>${dataSection.pregunta}</h1>`;
+
+    let btnValidate = document.getElementById("btnValidate");
 
     btnValidate.onclick = function() {
         validateRespuesta(dataSection.respuesta);
@@ -59,34 +72,107 @@ function loadSection (dataSection) {
 
 function loadCuestion(numCuestion) {
     let data = loadData();
-    document.getElementById("error").classList.remove("show");
+    document.getElementById("error").classList.add("hidden");
 
     if (Array.from(data).length >= numCuestion){
-        return loadSection(data[numCuestion]);
+        saveCookie(numCuestion);
+        return loadSection(data[numCuestion - 1]);
+    } else {
+        document.getElementById("preguntas").classList.add("hidden");
+        document.getElementById("endGame").classList.remove("hidden");
     }
-}
-
-function loadData() {
-    fetch('preguntas.json', {
-        method: 'GET',
-        mode: 'no-cors',
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        return data.preguntas;
-    })
-    .catch(error => {
-        console.log("error ", error);
-    });
 }
 
 function validateRespuesta(respuesta){
     let respuestaUser = document.getElementById("respuesta").value;
+    document.getElementById("respuesta").value = "";
+
+    document.getElementById("error").classList.add("hidden");
+    document.getElementById("correcto").classList.add("hidden");
     
     if (respuestaUser === respuesta){
-        loadCuestion(NUMCUESTIONNOW + 1);
+        document.getElementById("correcto").classList.remove("hidden");
+        loadCuestion(++NUMCUESTIONNOW);
+    } else {
+        document.getElementById("error").classList.remove("hidden");
+    }
+}
+
+function tristeOne(){
+    document.getElementById("endGame").classList.add("hidden");
+    document.getElementById("pasoOne").classList.remove("hidden");
+}
+
+function crecerAmor(){
+    let btnAmor = document.getElementById("amor");
+
+    amorWidth += 10;
+    amorHeight += 5;
+
+    btnAmor.style.width = `${amorWidth}px`;
+    btnAmor.style.height = `${amorHeight}px`;
+
+    let btnTristeza = document.getElementById("triste");
+
+    tristezaWidth -= 5;
+    tristezaHeight -= 2;
+
+    btnTristeza.style.width = `${tristezaWidth}px`;
+    btnTristeza.style.height = `${tristezaHeight}px`;
+
+    if (Number(btnAmor.style.width.replace("px", "")) >= window.innerWidth){
+        document.getElementById("endGame").classList.add("hidden");
+        document.getElementById("pasoOne").classList.add("hidden");
+        document.getElementById("pasoTwo").classList.remove("hidden");
+        document.getElementById("body").classList.add("fondoMuelle");
+    }
+}
+
+function pasoThree(){
+    document.getElementById("pasoTwo").classList.add("hidden");
+    document.getElementById("pasoThree").classList.remove("hidden");
+}
+
+function pasoFour(){
+    document.getElementById("pasoThree").classList.add("hidden");
+    document.getElementById("pasoFour").classList.remove("hidden");
+}
+
+function saveCookie(numCuestion){
+    // Define la fecha de expiración de la cookie (opcional)
+    let fechaExpiracion = new Date();
+    fechaExpiracion.setDate(fechaExpiracion.getDate() + 20);
+
+    let cookieString = "numCuestion=" + encodeURIComponent(numCuestion);
+    if (fechaExpiracion) {
+        cookieString += "; expires=" + fechaExpiracion.toUTCString();
     }
 
-    document.getElementById("error").classList.add("show");
+    // Asigna la cadena de cookie al documento
+    document.cookie = cookieString;
+}
+
+function obtenerCookie() {
+    let cookies = document.cookie.split("; ");
+
+    for (let i of cookies.length) {
+        let cookie = cookies[i].split("=");
+        if (cookie[0] === "numCuestion") {
+            return decodeURIComponent(cookie[1]);
+        }
+    }
+    return null; // Si no se encuentra la cookie
+}
+
+function loadData(){
+    return [
+        {
+            "pregunta": "esto es una prueba 1",
+            "respuesta": "test",
+        },
+        {
+            "pregunta": "esto es una prueba 2",
+            "respuesta": "test2",
+        }
+    ];
 }
